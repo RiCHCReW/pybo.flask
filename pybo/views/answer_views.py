940e3340dbdcb5b218999b1e0affcaed7124334f
DCCoder <heneicosa@gmail.com>
2024-02-02 13:56:25 +0900

    3-12 앵커 완료

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -22,7 +22,8 @@ def create(question_id):
         answer = Answer(content=content, create_date=datetime.now(), user=g.user)
         question.answer_set.append(answer)
         db.session.commit()
-        return redirect(url_for('question.detail', question_id=question_id))
+        return redirect('{}#answer_{}'.format(
+            url_for('question.detail', question_id=question_id), answer.id))
     return render_template('question/question_detail.html', question=question, form=form)
 
 
@@ -41,7 +42,8 @@ def modify(answer_id) :
             form.populate_obj(answer)
             answer.modify_date = datetime.now()  # 수정일시 저장
             db.session.commit()
-            return redirect(url_for('question.detail', question_id=answer.question.id))
+            return redirect('{}#answer_{}'.format(
+                url_for('question.detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(obj=answer)
     return render_template('answer/answer_form.html', form=form)
@@ -58,7 +60,8 @@ def delete(answer_id) :
     else :
         db.session.delete(answer)
         db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    return redirect('{}#answer_{}'.format(
+        url_for('question.detail', question_id=answer.question.id), answer.id))
 
 
 #--------------------------------------------------------------------------------------------------
