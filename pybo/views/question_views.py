26399c7538a35d677ff136b4e72b6b0893bf269e
DCCoder <heneicosa@gmail.com>
2024-02-02 11:16:49 +0900

    3-10 게시물 수정 & 삭제 완료

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -67,3 +67,16 @@ def modify(question_id) :
 
 
 #--------------------------------------------------------------------------------------------------
+@bp.route('/delete/<int:question_id>')
+@login_required
+def delete(question_id) :
+  question = Question.query.get_or_404(question_id)
+  if g.user != question.user :
+    flash('삭제 권한이 없습니다.')
+    return redirect(url_for('question.detail', question_id=question_id))
+  db.session.delete(question)
+  db.session.commit()
+  return redirect(url_for('question._list'))
+
+
+#--------------------------------------------------------------------------------------------------
